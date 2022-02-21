43f02f15beda212ce2b451a5bcd3e6cfeeb715d2
laaanberggg <84227247+laaanberggg@users.noreply.github.com>
2022-02-21 05:03:58 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace murzaev_mvc
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            pattern.tbPerviy = tbOne;
+            pattern.tbVtoroy = tbTwo;
+            pattern.tbItogoviy = tbItogoviy;
+            cbMoove.ItemsSource = pattern.operList;
+        }
+
+        private void buttRez_Click(object sender, RoutedEventArgs e)
+        {
+            pattern.One = Convert.ToInt32(tbOne.Text);
+            pattern.Two = Convert.ToInt32(tbTwo.Text);
+            pattern.cbMoove = cbMoove.SelectedIndex;
+            tbItogoviy.Text = pattern.Rez;
+        }
+        
+    }
+}
+
---
+++ pattern.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace murzaev_mvc
+{
+    class pattern
+    {
+        static int one = 0;
+        static int two = 0;
+        public static List<string> operList = new List<string> { "+", "-", "*", "/" };
+        static int operId = 0;
+
+        public static TextBox tbPerviy;
+        public static TextBox tbVtoroy;
+        public static TextBox tbItogoviy;
+
+        public static int One
+        {
+            set
+            {
+                one = value;
+            }
+        }
+        public static int Two
+        {
+            set
+            {
+                two = value;
+            }
+        }
+        public static int cbMoove
+        {
+            set
+            {
+                operId = value;
+            }
+        }
+        public static string Rez
+        {
+            get
+            {
+                switch (operId)
+                {
+                    case 0:
+                        return tbItogoviy.Text = (one + two).ToString();
+                    case 1:
+                        return tbItogoviy.Text = (one - two).ToString();
+                    case 2:
+                        return tbItogoviy.Text = (one * two).ToString();
+                    case 3:
+                        return tbItogoviy.Text = (one / two).ToString();
+                    default:
+                        return "!!!";
+                }
+            }
+        }
+    }
+}
